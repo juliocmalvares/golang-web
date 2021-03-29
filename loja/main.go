8f08b05dc3d6e7a3e564e8926f4e434316d03dfb
juliocmalvares <juliocmalvares07@gmail.com>
2021-03-29 20:23:56 -0300

    feat: :sparkles: Adicionando estrutura de produtos e melhorias no HTML

--- loja/main.go
+++ loja/main.go
@@ -7,11 +7,23 @@ import (
 
 var temp = template.Must(template.ParseGlob("./templates/*.html"))
 
+type Produto struct {
+	Nome, Descricao string
+	Preco           float64
+	Qtd             int32
+}
+
 func main() {
 	http.HandleFunc("/", index)
 	http.ListenAndServe(":8000", nil)
 }
 
 func index(w http.ResponseWriter, r *http.Request) {
-	temp.ExecuteTemplate(w, "index", nil)
+	produtos := []Produto{
+		{Nome: "Camiseta", Descricao: "Camiseta Bonita", Preco: 29.99, Qtd: 10},
+		{"Tênis", "Confortável", 299.99, 10},
+		{"Fone Bluetooth", "Muito bão", 59.99, 2},
+	}
+
+	temp.ExecuteTemplate(w, "index", produtos)
 }
